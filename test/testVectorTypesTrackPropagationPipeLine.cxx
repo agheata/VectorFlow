1df32b45fc9f02cb269d4862116cfe5fdd9f477e
agarzaro <arturo.garza.mail@gmail.com>
2019-07-08 16:02:03 -0700

    Changed test/testVectorTypestrackPropagationPipeLine.cxx to use vector types Double_v instead of AlignedArray to avoid extra copying step. Test is almost complete, vectorizing the operations to calculate the step is missing.

--- test/testVectorTypesTrackPropagationPipeLine.cxx
+++ test/testVectorTypesTrackPropagationPipeLine.cxx
@@ -11,9 +11,9 @@
 /// +--------------------------------------------------------------------------------------+
 
 #include <iostream>
-#include <string>
 #include <vector>
 #include <vectorFlow/PipelineFlow.h>
+#include <vecCore/vecCore>
 #include "CocktailGenerator.h"
 #include "Event.h"
 #include "Track.h"
@@ -22,42 +22,43 @@
 #include "base/Vector3D.h"
 #include "HelixPropagator.h"
 #include "SimpleStepper.h"
-#include "vectorFlow/AlignedArray.h"
 #include "vectorFlow/VectorTypes.h"
 #include "Geant/ConstFieldHelixStepper.h"
 
 using namespace vectorflow;
+using vecCore::Get;
+using vecCore::Set;
+using vecCore::Load;
+using vecCore::Store;
+using vecCore::Scalar;
 
-template <typename Data, typename DataContainer>
-struct PropagationSOA {
-  std::size_t fSize;
-  AlignedArray<double> fPosX   , fPosY    , fPosZ   ;
-  AlignedArray<double> fDirX   , fDirY    , fDirZ   ;
-  AlignedArray<double> fCharge , fMomentum, fStep   ;
-  AlignedArray<double> fNewPosX, fNewPosY , fNewPosZ;
-  AlignedArray<double> fNewDirX, fNewDirY , fNewDirZ;
+template <typename Data>
+struct SIMDTracks {
+  // Data in vecCore vector types
+  Double_v fPosX_v   , fPosY_v    , fPosZ_v   ;
+  Double_v fDirX_v   , fDirY_v    , fDirZ_v   ;
+  Double_v fCharge_v , fMomentum_v, fStep_v   ;
+  Double_v fNewPosX_v, fNewPosY_v , fNewPosZ_v;
+  Double_v fNewDirX_v, fNewDirY_v , fNewDirZ_v;
 
-  // Gather info to fill one SOA lane
+  // Gather info to fill one SIMD lane
   void Gather(Data* track, std::size_t lane) {
-    fPosX[lane]     = track->Position().x();
-    fPosY[lane]     = track->Position().y();
-    fPosZ[lane]     = track->Position().z();
-    fDirX[lane]     = track->Direction().x();
-    fDirY[lane]     = track->Direction().y();
-    fDirZ[lane]     = track->Direction().z();
-    fCharge[lane]   = track->Charge();
-    fMomentum[lane] = track->P();
-    fStep[lane]     = track->GetStep();
+    Set(fPosX_v    , lane, track->Position().x());
+    Set(fPosY_v    , lane, track->Position().y());
+    Set(fPosZ_v    , lane, track->Position().z());
+    Set(fDirX_v    , lane, track->Direction().x());
+    Set(fDirY_v    , lane, track->Direction().y());
+    Set(fDirZ_v    , lane, track->Direction().z());
+    Set(fCharge_v  , lane, track->Charge());
+    Set(fMomentum_v, lane, track->P());
+    Set(fStep_v    , lane, track->GetStep());
   }
 
   // Scatter one lane info back to original structure
   void Scatter(Data* track, std::size_t lane) {
-    track->SetPosition(fNewPosX[lane], fNewPosY[lane], fNewPosZ[lane]);
-    track->SetDirection(fNewDirX[lane], fNewDirY[lane], fNewDirZ[lane]);
+    track->SetPosition(static_cast<Scalar<Double_v>>(Get(fNewPosX_v, lane)), static_cast<Scalar<Double_v>>(Get(fNewPosY_v, lane)), static_cast<Scalar<Double_v>>(Get(fNewPosZ_v, lane)));
+    track->SetDirection(static_cast<Scalar<Double_v>>(Get(fNewDirX_v, lane)), static_cast<Scalar<Double_v>>(Get(fNewDirY_v, lane)), static_cast<Scalar<Double_v>>(Get(fNewDirZ_v, lane)));
   }
-
-  // Struct constructor
-  PropagationSOA(std::size_t size) : fSize(size), fPosX(size), fPosY(size), fPosZ(size), fDirX(size), fDirY(size), fDirZ(size), fCharge(size), fMomentum(size), fStep(size), fNewPosX(size), fNewPosY(size), fNewPosZ(size), fNewDirX(size), fNewDirY(size), fNewDirZ(size) {}
 };
 
 struct TaskPropagator : public Work<Track, std::vector<Track*>> {
@@ -65,6 +66,7 @@ struct TaskPropagator : public Work<Track, std::vector<Track*>> {
   trackml::HelixPropagator* fPropagator;
   trackml::SimpleStepper*   fStepper;
   ConstFieldHelixStepper*   fHelixStepper;
+  SIMDTracks<Track> fSIMDStruct;
 
   // Scalar mode executor
   void Execute(Track* track) {
@@ -73,125 +75,107 @@ struct TaskPropagator : public Work<Track, std::vector<Track*>> {
     fStepper->PropagateToR(radius, *track); 
   }
 
-  // Check whether all lanes can still be propagated or not
-  bool AllLanesCanDoStep(double* mask, const std::size_t& kVectorSize) {
+  // function to check whether all lanes can still be propagated or not
+  bool AllLanesCanDoStep(double* cvalues, const std::size_t& kVectorSize) {
     for (auto i = 0; i < kVectorSize; i++) 
-      if (mask[i] >= 0.) return false;
+      if (cvalues[i] >= 0.) return false;
     return true;
   }
 
   // PropagateToR function for a vector of tracks
-  template <typename Real_v>
-  void PropagateToR(std::vector<Track*> const& tracks) {
-    using vecCore::Load;
-    using vecCore::Store;
-    std::size_t tracksSize = tracks.size();
-    constexpr std::size_t kVectorSize = vecCore::VectorSize<Real_v>();
+  void PropagateToR(const double& radius, std::vector<Track*> const& tracks) {
+    const std::size_t kTracksSize = tracks.size();
+    const std::size_t kVectorSize = vecCore::VectorSize<Double_v>();
     
     // Execute scalar mode if there are less tracks than vector lanes
-    if (tracksSize < kVectorSize) {
-      for (auto i = 0; i < tracksSize; i++)
+    if (kTracksSize < kVectorSize) {
+      for (auto i = 0; i < kTracksSize; i++)
         Execute(tracks[i]);
       return;
     }
 
-    PropagationSOA<Track, std::vector<Track*>> mySOA(kVectorSize);
-
-    double mask[kVectorSize];      // Array to check if the track in the lane is fully propagated
-    std::size_t lane[kVectorSize]; // Array to check which track is executing in which lane
-    std::size_t nextTrack = 0;
+    double cvalues[kVectorSize];   // Auxiliar array to check if the track in the lane is fully propagated
+    std::size_t lane[kVectorSize]; // Auxiliar array to check which track is executing in which lane
+    std::size_t nextTrack = 0;     // Auxiliar counter to keep record of which track will be dispatched 
 
     // Declare constants and auxiliar variables
-    constexpr double radius      = 20.   * geant::units::cm;
-    constexpr double epsilon     = 1.E-4 * geant::units::mm;
-    constexpr double toKiloGauss = 1.0   / geant::units::kilogauss;
+    const double epsilon     = 1.E-4 * geant::units::mm;
+    const double toKiloGauss = 1.0   / geant::units::kilogauss;
 
     vecgeom::Vector3D<double> pos;
     vecgeom::Vector3D<double> dir;
     vecgeom::Vector3D<double> const bfield = fPropagator->GetBfield();
-    constexpr double radius2 = radius * radius;
+    const double radius2 = radius * radius;
     double rad2, c, safety, dmax, pDotV, d2, snext, step_geom, step_field, step;
 
     // Set up the first lanes 
     for (auto i = 0; i < kVectorSize; i++) {
-      lane[i] = i;
-      pos     = tracks[i]->Position();
-      dir     = tracks[i]->Direction();
-      rad2    = pos.Mag2();
-      c       = rad2 - radius2;
-      mask[i] = c;
+      lane[i]    = i;
+      pos        = tracks[i]->Position();
+      dir        = tracks[i]->Direction();
+      rad2       = pos.Mag2();
+      c          = rad2 - radius2;
+      cvalues[i] = c;
       nextTrack++;
     }
 
-    // Execute vector mode until there are no more tracks to dispatch or 
-    // all lanes can still be propagated
-    while (nextTrack < tracksSize || AllLanesCanDoStep(mask, kVectorSize)) {
-      // Update tracks state and auxiliar variables
+    // Execute in vector mode until there are no more tracks to dispatch
+    // or all lanes in SIMD struct can still be propagated
+    while (nextTrack < kTracksSize || AllLanesCanDoStep(cvalues, kVectorSize)) {
+      // Calculate track step
       for (auto i = 0; i < kVectorSize; i++) {
-        // mask[i] = c >= 0 means fully propagation
-        if (mask[i] >= 0. && nextTrack < tracksSize) { 
-          lane[i] = nextTrack;
-          pos  = tracks[nextTrack]->Position();
-          dir  = tracks[nextTrack]->Direction();
-          rad2 = pos.Mag2();
-          c    = rad2 - radius2;
-          mask[i] = c;
+        // cvalues[i] = c >= 0 means fully propagation
+        if (cvalues[i] >= 0. && nextTrack < kTracksSize) { 
+          // Assign new track to a lane 
+          lane[i]    = nextTrack;
+          pos        = tracks[nextTrack]->Position();
+          dir        = tracks[nextTrack]->Direction();
+          rad2       = pos.Mag2();
+          c          = rad2 - radius2;
+          cvalues[i] = c;
           nextTrack++;
         }
-        safety = radius - vectorflow::Math::Sqrt(rad2);
+        safety = radius - vecCore::math::Sqrt(rad2);
         dmax   = 8. * epsilon / tracks[lane[i]]->Curvature(bfield.z() * toKiloGauss);
         pDotV  = pos.Dot(dir);
-        d2     = pDotV * pDotV - mask[i];
-        snext  = -pDotV + vectorflow::Math::Sqrt(vectorflow::Math::Abs(d2));
+        d2     = pDotV * pDotV - cvalues[i];
+        snext  = -pDotV + vecCore::math::Sqrt(vecCore::math::Abs(d2));
+        
         tracks[lane[i]]->SetSafety(safety);
         tracks[lane[i]]->SetSnext(snext);
-        step_geom  = vectorflow::Math::Max(epsilon, tracks[lane[i]]->GetSnext());
-        step_field = vectorflow::Math::Max(dmax, safety);
-        step       = vectorflow::Math::Min(step_geom, step_field);
+        
+        step_geom  = vecCore::math::Max(epsilon, snext);
+        step_field = vecCore::math::Max(dmax, safety);
+        step       = vecCore::math::Min(step_geom, step_field);
+        
         tracks[lane[i]]->SetStep(step);
 
-        // Gather info into SOA type
-        mySOA.Gather(tracks[lane[i]], i);
+        // Gather info into SIMD struct
+        fSIMDStruct.Gather(tracks[lane[i]], i);
       }
-    
-      Real_v oldPosX_v, oldPosY_v, oldPosZ_v, oldDirX_v, oldDirY_v, oldDirZ_v;
-      Real_v newPosX_v, newPosY_v, newPosZ_v, newDirX_v, newDirY_v, newDirZ_v;
-      Real_v charge_v, momentum_v, stepSz_v;
 
-      Load(oldPosX_v, mySOA.fPosX.Head());
-      Load(oldPosY_v, mySOA.fPosY.Head());
-      Load(oldPosZ_v, mySOA.fPosZ.Head());
-      Load(oldDirX_v, mySOA.fDirX.Head());
-      Load(oldDirY_v, mySOA.fDirY.Head());
-      Load(oldDirZ_v, mySOA.fDirZ.Head());
-      Load(charge_v, mySOA.fCharge.Head());
-      Load(momentum_v, mySOA.fMomentum.Head());
-      Load(stepSz_v, mySOA.fStep.Head());
-
-      fHelixStepper->DoStep<Real_v>(oldPosX_v, oldPosY_v, oldPosZ_v, oldDirX_v, oldDirY_v, oldDirZ_v, charge_v, momentum_v, stepSz_v, newPosX_v, newPosY_v, newPosZ_v, newDirX_v, newDirY_v, newDirZ_v);
-
-      Store(newPosX_v, mySOA.fNewPosX.Head());
-      Store(newPosY_v, mySOA.fNewPosY.Head());
-      Store(newPosZ_v, mySOA.fNewPosZ.Head());
-      Store(newDirX_v, mySOA.fNewDirX.Head());
-      Store(newDirY_v, mySOA.fNewDirY.Head());
-      Store(newDirZ_v, mySOA.fNewDirZ.Head());
+      fHelixStepper->DoStep<Double_v>(fSIMDStruct.fPosX_v, fSIMDStruct.fPosY_v, fSIMDStruct.fPosZ_v, fSIMDStruct.fDirX_v, fSIMDStruct.fDirY_v, fSIMDStruct.fDirZ_v, fSIMDStruct.fCharge_v, fSIMDStruct.fMomentum_v, fSIMDStruct.fStep_v, fSIMDStruct.fNewPosX_v, fSIMDStruct.fNewPosY_v, fSIMDStruct.fNewPosZ_v, fSIMDStruct.fNewDirX_v, fSIMDStruct.fNewDirY_v, fSIMDStruct.fNewDirZ_v);
 
       // Update tracks state and auxiliar variables
       for (auto i = 0; i < kVectorSize; i++) {
         // Scatter back info into original structure
-        mySOA.Scatter(tracks[lane[i]], i);
+        fSIMDStruct.Scatter(tracks[lane[i]], i);
 
         tracks[lane[i]]->DecreasePstep(tracks[lane[i]]->GetStep());
         tracks[lane[i]]->DecreaseSnext(tracks[lane[i]]->GetStep());
         tracks[lane[i]]->DecreaseSafety(tracks[lane[i]]->GetStep());
         tracks[lane[i]]->IncreaseStep(tracks[lane[i]]->GetStep());
         tracks[lane[i]]->IncrementNsteps();
-
+        
         pos = tracks[lane[i]]->Position();
         dir = tracks[lane[i]]->Direction();
-        rad2 = pos.Mag2();
-        mask[i] = rad2 - radius2;
+
+        // Check direction is normalized
+        assert(dir.IsNormalized() && "ERROR: Direction not normalized after field propagation");
+
+        rad2       = pos.Mag2();
+        c          = rad2 - radius2;
+        cvalues[i] = rad2 - radius2;
       }
     }
 
@@ -203,16 +187,18 @@ struct TaskPropagator : public Work<Track, std::vector<Track*>> {
 
   // Vector mode executor
   void Execute(std::vector<Track*> const& tracks) {
-    std::cout << "Execution in vector mode.\n";
-    PropagateToR<Double_v>(tracks);
+    std::cout << "--EXECUTION IN VECTOR MODE--\n";
+    // Propagate track to the boundary of a sphere of radius 20cm
+    const double radius = 20. * geant::units::cm;
+    PropagateToR(radius, tracks);
     for (const auto& track : tracks) {
       std::cout << "Track " << track->Event() << " made " << track->GetNsteps() << " steps. ";
       std::cout << "Exit position: " << track->Position() << "\n";
     }
   }
 
-  // Struct constructor
-  TaskPropagator(trackml::HelixPropagator* propagator, trackml::SimpleStepper* stepper, ConstFieldHelixStepper* helixstepper) : fPropagator(propagator), fStepper(stepper), fHelixStepper(helixstepper) {}
+  // Task struct constructor
+  TaskPropagator(trackml::HelixPropagator* propagator, trackml::SimpleStepper* stepper, ConstFieldHelixStepper* helixstepper, SIMDTracks<Track> simd) : fPropagator(propagator), fStepper(stepper), fHelixStepper(helixstepper), fSIMDStruct(simd) {}
 };
 
 int main(int argc, char* argv[]) {
@@ -247,11 +233,14 @@ int main(int argc, char* argv[]) {
   trackml::SimpleStepper*   stepper      = new trackml::SimpleStepper(propagator);
   ConstFieldHelixStepper*   helixstepper = new ConstFieldHelixStepper(bfield);
 
+  // Create SIMD struct to handle vectorization
+  SIMDTracks<Track> mySIMDStruct;
+
   // Create a pipeline flow with one stage
   PipelineFlow<Track, std::vector<Track*>, 1> plFlow;
 
   // Create and initialize the propagator task
-  TaskPropagator tPropagate(propagator, stepper, helixstepper);
+  TaskPropagator tPropagate(propagator, stepper, helixstepper, mySIMDStruct);
 
   // Add the task to the flow
   static constexpr size_t kPropagatorStage = 0;
@@ -281,9 +270,10 @@ int main(int argc, char* argv[]) {
     event->Clear();
   }
 
-  // Clearing created pointers
+  // Clear created pointers
   delete stepper;
   delete propagator;
+  delete helixstepper;
 
   // End of test
   std::cout << "\n--END OF TEST--\n";
