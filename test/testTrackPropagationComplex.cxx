b94ccfe60ab73c07ec2bcb66e4f298afcf68acb9
Andrei Gheata <Andrei.Gheata@cern.ch>
2019-08-07 11:07:38 +0200

    Added propagation example with complex flow in scalar mode

--- test/testTrackPropagationComplex.cxx
+++ test/testTrackPropagationComplex.cxx
@@ -1,19 +1,20 @@
 /// +--------------------------------------------------------------------------------------+
 /// | VectorFlow unit test. Generation + Propagation.                                      |
 /// |                                                                                      |
-/// | This test is intended to show the usage of the PipelineFlow interface of VectorFlow. |
+/// | This test is intended to show the usage of the ComplexFlow interface of VectorFlow.  |
 /// | The example propagates charged particles in a constant field in several steps,       |
-/// | until they reach a sphere with a given radius centered in the event vertex.          |
+/// | in a geometry of adjacent concentric cylindrical layers                              |
 /// |                                                                                      |
-/// | The pipeline flow has only one task (TaskPropagator) and the data type to be used    |
-/// | are the tracks that are generated in each even of the event-loop.                    |
-/// | After each execution, the pipeline is cleared and the next tracks are processed.     |
+/// | The pipeline flow has only one task (TaskLayerPropagator) with multiple instances    |
+/// | (one per layer). Each task has up to 2 client tasks (the inner and/or outer layer    |
+/// | task). The data type to be used are the tracks that are generated in each even of    |
+/// | the event-loop.                                                                      |
 /// +--------------------------------------------------------------------------------------+
 
 #include <iostream>
 #include <string>
 #include <vector>
-#include <vectorFlow/PipelineFlow.h>
+#include <vectorFlow/ComplexFlow.h>
 #include "CocktailGenerator.h"
 #include "Event.h"
 #include "Track.h"
@@ -26,18 +27,27 @@
 using namespace vectorflow;
 
 //struct TaskPropagator : public Work<Event_t, std::vector<Event_t*>> {
-struct TaskPropagator : public Work<Track, std::vector<Track*>> {
+struct TaskLayerPropagator : public Work<Track, std::vector<Track*>> {
   // Propagator task needs a stepper
-  trackml::SimpleStepper* fStepper;
+  int fLayer = -1;
+  trackml::SimpleStepper* fStepper = nullptr;
 
   // Scalar mode executor
   void Execute(Track* track) {
-    // Propagate track to the boundary of a sphere of radius 20cm
-    constexpr double radius = 20. * geant::units::cm;
-    fStepper->PropagateToR(radius, *track); 
-    std::cout << "Track " << track->PrimaryParticleIndex() << " from event " << track->Event()
-              << " made " << track->GetNsteps() << " steps. ";
-    std::cout << "Exit position: " << track->Position() << "\n";
+    // Propagate track to the boundary of the current cylindrical layer
+    fStepper->PropagateInTube(fLayer, *track);
+    if (track->Status() == vectorflow::kKilled) {
+      std::cout << "Track " << track->PrimaryParticleIndex() << " from event " << track->Event()
+                << " made " << track->GetNsteps() << " steps. ";
+      std::cout << "Exit position: " << track->Position() << "\n";
+      return;
+    }
+    // Dispatch to next layer. Client 0 corresponds to the inner neighbour layer, client 1 to the outer one
+    bool move_outer = track->Position().Dot(track->Direction()) > 0;
+    size_t client = 0;
+    if (move_outer && fLayer > 0 && fLayer < 3) client = 1;
+
+    Dispatch(track, client);
   }
 
   // Vector mode executor
@@ -46,10 +56,11 @@ struct TaskPropagator : public Work<Track, std::vector<Track*>> {
   }
 
   // Struct constructor
-  TaskPropagator(trackml::SimpleStepper* stepper) : fStepper(stepper) {}
+  TaskLayerPropagator(int layer, trackml::SimpleStepper* stepper) : fLayer(layer), fStepper(stepper) {}
 };
 
 int main(int argc, char* argv[]) {
+  using namespace geant::units;
   // Read number of events to be generated
   int nEvents = argv[1] == NULL ? 10 : atoi(argv[1]);
 
@@ -63,7 +74,7 @@ int main(int argc, char* argv[]) {
   cocktailGen.AddPrimary("gamma", 0.4);
 
   // Set parameters of the generator
-  cocktailGen.SetPrimaryEnergyRange(0.1 * geant::units::GeV, 10 * geant::units::GeV);
+  cocktailGen.SetPrimaryEnergyRange(0.1 * GeV, 10 * GeV);
   cocktailGen.SetMaxPrimaryPerEvt(100);
   cocktailGen.SetAvgPrimaryPerEvt(70);
   cocktailGen.SetVertex(vertex);
@@ -76,19 +87,28 @@ int main(int argc, char* argv[]) {
   }
 
   // Initialize an helix propagator in field and a simple stepper
-  vecgeom::Vector3D<double> bfield(0., 0., 20. * geant::units::kilogauss);
+  vecgeom::Vector3D<double> bfield(0., 0., 20. * kilogauss);
   trackml::HelixPropagator* propagator = new trackml::HelixPropagator(bfield);
   trackml::SimpleStepper*   stepper    = new trackml::SimpleStepper(propagator);
 
-  // Create a pipeline flow with one stage
-  PipelineFlow<Track, std::vector<Track*>, 1> plFlow;
+  // Create a complex flow where each geometry layer is a stage
+  using LayerFlow_t = ComplexFlow<Track, std::vector<Track *>, 4>;
 
-  // Create and initialize the propagator task
-  TaskPropagator tPropagate(stepper);
+  LayerFlow_t flow;
 
-  // Add the task to the flow
-  static constexpr size_t kPropagatorStage = 0;
-  plFlow.AddWork(&tPropagate, kPropagatorStage);
+  // Create and initialize the layer propagator tasks
+  TaskLayerPropagator *propagators[4];
+  for (auto i = 0; i < 4; ++i) {
+    propagators[i] = new TaskLayerPropagator(i, stepper);
+    flow.AddWork(propagators[i], i);
+    flow.SetVectorMode(i, false); // scalar flow
+  }
+
+  // Connect clients to tasks
+  for (auto i = 0; i < 4; ++i) {
+    if (i > 0) propagators[i]->AddClient(&flow.InputData(i - 1));
+    if (i < 3) propagators[i]->AddClient(&flow.InputData(i + 1));
+  }
 
   // Event loop
   for (auto i = 0; i < nEvents; i++) {
@@ -100,20 +120,21 @@ int main(int argc, char* argv[]) {
     for (auto j = 0; j < event->GetNprimaries(); j++) {
       Track* track = event->GetPrimary(j);
       // Only charged tracks will be added
-      if (track->Charge() != 0) plFlow.AddData(track);
+      if (track->Charge() != 0) flow.AddData(track);
     }
 
-    // Process the flow
-    plFlow.Execute();
+    /// Process the complex flow as long as there is still data in the buffers
+    while (flow.GetNstates())
+      flow.Execute();
 
-    // Clear pipeline and event
-    plFlow.Clear();
+    // Clear the event
     event->Clear();
   }
 
   // Clearing created pointers
   delete stepper;
   delete propagator;
+  for (auto i = 0; i < 4; ++i) delete propagators[i];
 
   // End of test
   std::cout << "\n--END OF TEST--\n";
